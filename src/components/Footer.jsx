20db6e242bfb62677933d6d9c179722495e8c11e
IMrUnknown404I <ruslansubaev@yandex.ru>
2021-12-22 16:27:21 +0300

    #let

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer className='footer'>
-            (c) SR-Pic 2022
+            (c) SR-PIC 2022
         </footer>
     );
 };
--- src/components/Sider.jsx
+++ src/components/Sider.jsx
@@ -1,9 +1,90 @@
 import React from 'react';
 
 const Sider = () => {
+    let timer = 0;
+
+    function randomInteger(min, max) {
+        // получить случайное число от (min-0.5) до (max+0.5)
+        let rand = min - 0.5 + Math.random() * (max - min + 1);
+        return Math.round(rand);
+    }
+    function getRandomInner(rand) {
+        return rand > 0.2
+            ? rand > 0.4
+            ?  rand > 0.6
+                ? rand > 0.8
+                    ? '♻'
+                    : '⚝'
+                : '⨷'
+            :'⚠'
+            :'♾';
+    }
+
+    const getI = () => {
+        const currentId = 'num-'+timer;
+        timer++;
+        setInterval(() => {
+            const rand = Math.random();
+            document.querySelector(`.${currentId}`).innerHTML = getRandomInner(rand);
+        }, randomInteger(1500, 3500));
+
+        return <i className={currentId}>
+            {getRandomInner(Math.random())}
+        </i>
+    }
+
+    const getRain = () => {
+        return <div className='num-rain'>
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+            {getI()}
+        </div>
+    }
+
     return (
         <div className='sider'>
+            {getRain()}
 
+            <div className='sider-content'>
+
+            </div>
         </div>
     );
 };
