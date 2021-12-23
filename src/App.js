b243228583f6fd893d26cdfacb286f27770d2e17
IMrUnknown404I <ruslansubaev@yandex.ru>
2021-12-23 20:27:14 +0300

    #main-cont-fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './styles/main.css'
 import React from "react";
-import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Sider from "./components/Sider";
 import './utils/snowing'
@@ -9,7 +8,6 @@ import MainPage from "./components/pages/MainPage";
 function App() {
   return (
     <div className="App">
-      <Header/>
       <Sider/>
         <MainPage/>
       <Footer/>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 
-const Header = () => {
+const Header = ({pageId}) => {
+    const [text, className] = function(){
+        switch (pageId) {
+            case 'main': {
+                return ['ϺØtion ÐesÍgƝ', 'header-main']
+            }
+            case 'chap1': {
+                return ['1 Раздел', 'header-1c']
+            }
+            case 'chap2': {
+                return ['2 Раздел', 'header-2c']
+            }
+            case 'chap3': {
+                return ['3 Раздел', 'header-3c']
+            }
+            case 'chap4': {
+                return ['4 Раздел', 'header-4c']
+            }
+            case 'chap5': {
+                return ['5 Раздел', 'header-5c']
+            }
+        }
+    }();
+
     return (
-        <header className='header'>
+        <header className={'header ' + className}>
             {/*<img src='/images/gif/header-gif.gif' className='header-gif'/>*/}
             <div className='glitched-text'>
-                ϺØtion ÐesÍgƝ
+                {text}
+                {/*{pageId === 'main'*/}
+                {/*    ? 'ϺØtion ÐesÍgƝ'*/}
+                {/*    : pageId === '1chap'*/}
+                {/*        ? '1 Раздел'*/}
+                {/*        : pageId === '2chap'*/}
+                {/*            ? '2 Раздел'*/}
+                {/*            : pageId === '3chap'*/}
+                {/*                ? '3 Раздел'*/}
+                {/*                : pageId === '4chap'*/}
+                {/*                    ? '4 Раздел'*/}
+                {/*                    : '5 Раздел'*/}
+                {/*}*/}
+                {/*ϺØtion ÐesÍgƝ*/}
             </div>
         </header>
     );
